90ee1f0a244eb3157ea7ca4b764d2bf07040f848
Victoria Tuady <yayavictoria82@gmail.com>
2024-12-11 16:35:26 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![mobile view](design/mobile-view.png)
+![desktop view](design/desktop-view.png)
+
+### Links
+
+- Solution URL: [Solution URL](https://your-solution-url.com)
+- Live Site URL: [Live site URL](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- JavaScript
+
+## Author
+
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/yourusername)
---
+++ main.js
@@ -0,0 +1,80 @@
+const bill = document.getElementById("bill");
+const tips = document.querySelectorAll(".tips p");
+const customInput = document.getElementById("custom");
+const numOfPeople = document.getElementById("numOfPeople");
+const errorMessage = document.getElementById("errorMsg");
+const tipAmount = document.getElementById("tipAmount");
+const total = document.getElementById("total");
+const button = document.getElementById("btn");
+
+// create an array to store the values
+const values = [5, 10, 15, 25, 50];
+let storedValue;
+
+//Loop through the Ptags and assign an event listener
+tips.forEach((tip, index) => {
+  tip.addEventListener("click", () => {
+    tip.style.backgroundColor = "#26c0ab";
+    tip.style.color = "#00494d";
+
+    storedValue = values[index];
+    // console.log(`Selected values: ${storedValue}`);
+  });
+});
+
+//Event listener to display results
+numOfPeople.addEventListener("input", (e) => {
+  const inputValue = e.target.value;
+
+  const billInputValue = parseFloat(bill.value);
+  const numOfPeopleValue = parseInt(numOfPeople.value);
+  const customValue = parseInt(customInput.value);
+
+  //function to calculate tip amount
+  function calculateTipAmount(billInputValue, numOfPeopleValue, customValue) {
+    let tipValue;
+    if (storedValue) {
+      tipValue = storedValue;
+    } else {
+      tipValue = customValue;
+    }
+
+    //Calculate the tip amount
+    const totalTipAmount = (billInputValue * tipValue) / 100;
+
+    //calculate the total amount per head
+    const tipPerPerson = totalTipAmount / numOfPeopleValue;
+    // console.log(tipPerPerson);
+    // console.log(tipValue);
+    return tipPerPerson;
+  }
+
+  const tipResult = calculateTipAmount(
+    billInputValue,
+    numOfPeopleValue,
+    customValue
+  );
+
+  //function to calculate total amount
+  function calculateTotal(billInputValue, numOfPeopleValue) {
+    const totalAmount = billInputValue / numOfPeopleValue + tipResult;
+    // console.log(totalAmount);
+    return totalAmount;
+  }
+  const totalResult = calculateTotal(billInputValue, numOfPeopleValue);
+
+  //   Validate input value
+  if (inputValue <= 0) {
+    numOfPeople.style.border = "2px solid #ff7a7a";
+    errorMessage.style.display = "block";
+
+    setTimeout(() => {
+      numOfPeople.style.border = "none";
+      errorMessage.style.display = "none";
+    }, 2000);
+  } else {
+    tipAmount.textContent = `$${tipResult.toFixed(2)}`;
+    total.textContent = `$${totalResult.toFixed(2)}`;
+    button.style.backgroundColor = "#26c0ab";
+  }
+});
